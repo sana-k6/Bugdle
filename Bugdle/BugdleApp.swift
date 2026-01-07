4b7e9e5f6b1357f538730ad4a6e134d8c497ab2e
“Sana” <sanaakulkarny@gmail.com>
2026-01-07 11:31:15 +0400

    Initial Commit

---
+++ Bugdle/BugdleApp.swift
@@ -0,0 +1,17 @@
+//
+//  BugdleApp.swift
+//  Bugdle
+//
+//  Created by Sana Kulkarni on 07/01/2026.
+//
+
+import SwiftUI
+
+@main
+struct BugdleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Bugdle/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Bugdle
+//
+//  Created by Sana Kulkarni on 07/01/2026.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
