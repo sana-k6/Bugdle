569354a94b210a907e651d82f0c01113bec0a872
“Sana” <sanaakulkarny@gmail.com>
2026-04-03 12:50:30 +0100

    made puzzles.json and filled it with all the puzzle attributes

--- scripts/process_dataset.py
+++ scripts/process_dataset.py
@@ -1,8 +1,26 @@
+import json
 from datasets import load_dataset
 
 ds = load_dataset("Rtian/DebugBench")
 
+# check language values first
+puzzles = []
+for puzzle in ds["test"]:
+    if puzzle["language"] in ["python3", "java"]:
+        puzzles.append({
+            "slug": puzzle["slug"],
+            "category": puzzle["category"],
+            "subtype": puzzle["subtype"],
+            "language": puzzle["language"],
+            "level": puzzle["level"],
+            "release_time": puzzle["release_time"],
+            "question": puzzle["question"],
+            "examples": puzzle["examples"],
+            "constraints": puzzle["constraints"],
+            "solution": puzzle["solution"],
+            "solution_explanation": puzzle["solution_explanation"],
+            "buggy_code": puzzle["buggy_code"],
+            "bug_explanation": puzzle["bug_explanation"],
+        })
 with open("data/puzzles.json", "w") as f:
-    for item in ds["test"]:
-        if(item["language"] == "python" or item["language"] == "java"):
-            f.write(str(item) + "\n")
+    json.dump(puzzles, f, indent=4)
